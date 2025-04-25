11cfcc3beede6e2a015218a0e8a25792af198398
Kim_Vyacheslav_auca <93674837+kim-v-auca-2022@users.noreply.github.com>
2025-04-25 11:38:50 +0600

    Create send_request.py

---
+++ .github/workflows/send_request.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import requests
+
+# Converting code into json package
+f = open("sendtest1.cpp", "r")      # change to specific file path later
+file_string = f.read()
+cpp_code = """""" + file_string
+
+data = {
+        "model_name": "deepseek-r1:7b",
+        "code": cpp_code
+        }
+
+# Sending response
+response = requests.post("http://auca.space:7654/submit", json=data)
+print(response.json())
