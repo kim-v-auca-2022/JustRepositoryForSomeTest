9e890a6f41a02b3174456f45b29ee2d2541cc628
Kim_Vyacheslav_auca <93674837+kim-v-auca-2022@users.noreply.github.com>
2025-04-25 12:05:16 +0600

    Update send_request.py (no print cpp_code)

--- .github/workflows/send_request.py
+++ .github/workflows/send_request.py
@@ -11,8 +11,6 @@ data = {
         "code": cpp_code
         }
 
-print(cpp_code)
-
 # Sending response (if server exists)
 try:
     response = requests.post("http://auca.space:7654/submit", json=data)
